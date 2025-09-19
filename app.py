7c01d6642a3d11c885956edec492e29312d3cd1d
Mamun Ahammed <m.cseiu05@gmail.com>
2025-09-19 19:20:08 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-from sklearn.datasets import make_blobs
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
+
+
 
 st.title("🔍 K-Means Explorer")
 
